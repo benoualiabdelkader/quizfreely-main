020e1524b521bdfc97d2af169cb640ff0cb44d99
benoualiabdelkader <abdelkaderbenouali301@gmail.com>
2026-08-13 03:27:47 +0100

    feat: add PWA and offline service worker support

---
+++ src/service-worker.js
@@ -0,0 +1,78 @@
+import { build, files, version } from '$service-worker';
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files  // everything in `static`
+];
+
+self.addEventListener('install', (event) => {
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+	// Remove previous cached data from disk
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+	// ignore POST requests etc
+	if (event.request.method !== 'GET') return;
+
+    // Ignore requests to external APIs like the graphql endpoint
+    const url = new URL(event.request.url);
+    if (url.origin !== self.location.origin) return;
+
+	async function respond() {
+		const cache = await caches.open(CACHE);
+
+		// `build`/`files` can always be served from the cache
+		if (ASSETS.includes(url.pathname)) {
+			const cachedResponse = await cache.match(url.pathname);
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+		}
+
+		// for everything else, try the network first, but
+		// fall back to the cache if we're offline
+		try {
+			const response = await fetch(event.request);
+
+            // if we're offline, fetch can return a value that is not a Response
+            // instead of throwing - and we can't pass this non-Response to respondWith
+            if (!(response instanceof Response)) {
+                throw new Error('invalid response from fetch');
+            }
+            
+			if (response.status === 200) {
+				cache.put(event.request, response.clone());
+			}
+
+			return response;
+		} catch {
+			const cachedResponse = await cache.match(event.request);
+            if (cachedResponse) return cachedResponse;
+            
+            // if we are fully offline and the cache misses, it's safer to just throw
+            // however, sveltekit's offline page can be served if desired.
+            throw new Error('Offline and not in cache');
+		}
+	}
+
+	event.respondWith(respond());
+});
